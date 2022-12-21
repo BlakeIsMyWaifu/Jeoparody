a88e8cce903cb81078ecd354caf4e5e50d436a6e
BlakeIsMyWaifu <33188727+BlakeIsMyWaifu@users.noreply.github.com>
2022-12-21 09:49:49 +0000

    🎉 Init

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,28 @@
-import { type AppType } from "next/app";
+import { MantineProvider } from '@mantine/core'
+import { type AppProps, type AppType } from 'next/app'
+import Head from 'next/head'
 
-import { trpc } from "../utils/trpc";
+import { trpc } from 'utils/trpc'
 
-import "../styles/globals.css";
+const MyApp: AppType = ({ Component, pageProps }: AppProps) => {
+	return <>
+		<Head>
+			<title>Jeoparody</title>
+			<link rel='icon' href='/favicon.ico' />
+			<meta charSet='UTF-8' />
+			<meta name='viewport' content='width=device-width, initial-scale=1.0' />
+		</Head>
 
-const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
-};
+		<MantineProvider
+			withGlobalStyles
+			withNormalizeCSS
+			theme={{
+				colorScheme: 'dark'
+			}}
+		>
+			<Component {...pageProps} />
+		</MantineProvider>
+	</>
+}
 
-export default trpc.withTRPC(MyApp);
+export default trpc.withTRPC(MyApp)
--- src/utils/trpc.ts
+++ src/utils/trpc.ts
@@ -1,42 +1,58 @@
-import { httpBatchLink, loggerLink } from "@trpc/client";
-import { createTRPCNext } from "@trpc/next";
-import { type inferRouterInputs, type inferRouterOutputs } from "@trpc/server";
-import superjson from "superjson";
+import { createWSClient, httpBatchLink, loggerLink, type TRPCLink, wsLink } from '@trpc/client'
+import { createTRPCNext } from '@trpc/next'
+import { type AnyRouter, type inferRouterInputs, type inferRouterOutputs } from '@trpc/server'
+import { type NextPageContext } from 'next'
+import getConfig from 'next/config'
+import superjson from 'superjson'
 
-import { type AppRouter } from "../server/trpc/router/_app";
+import { type AppRouter } from 'server/trpc/router/_app'
 
-const getBaseUrl = () => {
-  if (typeof window !== "undefined") return ""; // browser should use relative url
-  if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`; // SSR should use vercel url
-  return `http://localhost:${process.env.PORT ?? 3000}`; // dev SSR should use localhost
-};
+const { publicRuntimeConfig } = getConfig()
+
+const { APP_URL, WS_URL } = publicRuntimeConfig
+
+function getEndingLink(ctx: NextPageContext | undefined): TRPCLink<AnyRouter> {
+	if (typeof window === 'undefined') {
+		return httpBatchLink({
+			url: `${APP_URL}/api/trpc`,
+			headers() {
+				if (ctx?.req) {
+					return {
+						...ctx.req.headers,
+						'x-ssr': '1'
+					}
+				}
+				return {}
+			}
+		})
+	}
+	const client = createWSClient({
+		url: WS_URL
+	})
+	return wsLink<AppRouter>({
+		client
+	})
+}
 
 export const trpc = createTRPCNext<AppRouter>({
-  config() {
-    return {
-      transformer: superjson,
-      links: [
-        loggerLink({
-          enabled: (opts) =>
-            process.env.NODE_ENV === "development" ||
-            (opts.direction === "down" && opts.result instanceof Error),
-        }),
-        httpBatchLink({
-          url: `${getBaseUrl()}/api/trpc`,
-        }),
-      ],
-    };
-  },
-  ssr: false,
-});
+	config({ ctx }) {
+		return {
+			links: [
+				loggerLink({
+					enabled: opts =>
+						(process.env.NODE_ENV === 'development' &&
+							typeof window !== 'undefined') ||
+						(opts.direction === 'down' && opts.result instanceof Error)
+				}),
+				getEndingLink(ctx)
+			],
+			transformer: superjson,
+			queryClientConfig: { defaultOptions: { queries: { staleTime: 60 } } }
+		}
+	},
+	ssr: true
+})
 
-/**
- * Inference helper for inputs
- * @example type HelloInput = RouterInputs['example']['hello']
- **/
-export type RouterInputs = inferRouterInputs<AppRouter>;
-/**
- * Inference helper for outputs
- * @example type HelloOutput = RouterOutputs['example']['hello']
- **/
-export type RouterOutputs = inferRouterOutputs<AppRouter>;
+export type RouterInputs = inferRouterInputs<AppRouter>
+
+export type RouterOutputs = inferRouterOutputs<AppRouter>
