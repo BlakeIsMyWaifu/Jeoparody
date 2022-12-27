ae7c6b17b4143cef315677fd4a1e633eb1e0abbc
BlakeIsMyWaifu <33188727+BlakeIsMyWaifu@users.noreply.github.com>
2022-12-27 16:44:52 +0000

    🎨 Improved home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,37 @@
 import { type NextPage } from 'next'
-import { Button, Modal, Title, useMantineTheme, Stack, Group, TextInput } from '@mantine/core'
+import { Button, Modal, Title, Stack, Group, TextInput } from '@mantine/core'
 import { useHostStore } from 'state/hostClientStore'
-import { useState } from 'react'
+import { useState, type FC } from 'react'
 import { trpc } from 'utils/trpc'
 import { usePlayerStore } from 'state/playerClientStore'
 import { useRouter } from 'next/router'
+import { useInputState } from '@mantine/hooks'
 
 const HomePage: NextPage = () => {
+	return (
+		<Modal
+			centered
+			overflow='inside'
+			withCloseButton={false}
+			opened={true}
+			onClose={() => undefined}
+		>
+			<Stack>
+				<Title>Jeoparody</Title>
+				<Player />
+				<Host />
+			</Stack>
+		</Modal>
+	)
+}
 
-	const theme = useMantineTheme()
+const Player: FC = () => {
 
 	const router = useRouter()
 
-	// Player
-	const playerActive = usePlayerStore(state => state.isPlayer)
 	const setPlayer = usePlayerStore(state => state.setPlayer)
 
-	const [playerNameInput, setPlayerNameInput] = useState('')
+	const [playerNameInput, setPlayerNameInput] = useInputState('')
 	const [playerNameInputDisabledMessage, setPlayerNameInputDisabledMessage] = useState<false | string>(false)
 
 	const player = trpc.room.becomePlayer.useMutation({
@@ -30,8 +45,27 @@ const HomePage: NextPage = () => {
 		}
 	})
 
-	// Host
-	const hostActive = useHostStore(state => state.isHost)
+	return (
+		<Group>
+			<TextInput
+				value={playerNameInput}
+				onChange={setPlayerNameInput}
+				placeholder='Your Name'
+				label='Player Name'
+				error={playerNameInputDisabledMessage}
+			/>
+			<Button onClick={() => {
+				setPlayerNameInputDisabledMessage(false)
+				player.mutate(playerNameInput)
+			}}>Player</Button>
+		</Group>
+	)
+}
+
+const Host: FC = () => {
+
+	const router = useRouter()
+
 	const becomeHost = useHostStore(state => state.becomeHost)
 
 	const host = trpc.room.becomeHost.useMutation({
@@ -42,36 +76,11 @@ const HomePage: NextPage = () => {
 		}
 	})
 
-	return !hostActive && !playerActive ? (
-		<Modal
-			centered
-			overflow='inside'
-			overlayColor={theme.colorScheme === 'dark' ? theme.colors.dark[9] : theme.colors.gray[2]}
-			withCloseButton={false}
-			opened={true}
-			onClose={() => undefined}
-		>
-			<Stack>
-				<Title>Jeoparody</Title>
-				<Group>
-					<TextInput
-						value={playerNameInput}
-						onChange={event => setPlayerNameInput(event.currentTarget.value)}
-						placeholder='Your Name'
-						label='Player Name'
-						error={playerNameInputDisabledMessage}
-					/>
-					<Button onClick={() => {
-						setPlayerNameInputDisabledMessage(false)
-						player.mutate(playerNameInput)
-					}}>Player</Button>
-				</Group>
-				<Button onClick={() => {
-					host.mutate()
-				}}>Host</Button>
-			</Stack>
-		</Modal>
-	) : null
+	return (
+		<Button onClick={() => {
+			host.mutate()
+		}}>Host</Button>
+	)
 }
 
 export default HomePage
